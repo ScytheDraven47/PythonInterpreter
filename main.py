0cefbc6ce8b6d3051cea01ccfce250f07056cbbc
ScytheDraven47 <ben.rogers.mckee@gmail.com>
2017-05-08 18:42:19 +1200

    v2.02 - Created fix for Duplicate Code smell, implemented 2 of 12.

--- main.py
+++ main.py
@@ -55,29 +55,24 @@ class MainFlow(cmd.Cmd):
         [-d] Pulls from chosen database
         [-e] Pulls from chosen excel spreadsheet
         """
-        args = line.split()
-        if len(args) == 1:
-            if args[0] == "-d":
-                view = DatabaseView()
-                pull_file = self.database_name
-            elif args[0] == "-e":
-                view = ExcelView()
-                pull_file = self.excel_file
-            else:
-                ic.log.output("Incorrect flag. Only '-d' or '-e' are valid. ")
-                return 2
-            try:
-                ic.get_data(pull_file, view)
-                return 0
-            except FileNotFoundError:
-                ic.log.output("File does not exist... Please use 'change_data_source [-d|-e]' to edit data source.")
-                return 3
-            except KeyError:
-                ic.log.output("File exists, but there is no worksheet labelled 'input'.")
-                return 4
+        pull_flag = self.valid_flag(line, ["-d", "-e"], 1, self.do_pull_data.__doc__)
+        if type(pull_flag) is int:
+            return pull_flag
+        elif pull_flag == "-d":
+            view = DatabaseView()
+            pull_file = self.database_name
         else:
-            ic.log.output("Incorrect syntax." + str(self.do_pull_data.__doc__))
-            return 1
+            view = ExcelView()
+            pull_file = self.excel_file
+        try:
+            ic.get_data(pull_file, view)
+            return 0
+        except FileNotFoundError:
+            ic.log.output("File does not exist... Please use 'change_data_source [-d|-e]' to edit data source.")
+            return 3
+        except KeyError:
+            ic.log.output("File exists, but there is no worksheet labelled 'input'.")
+            return 4
 
     def do_push_data(self, line):
         """
@@ -86,10 +81,10 @@ class MainFlow(cmd.Cmd):
         [-d] Saves to chosen database
         [-e] Saves to chosen excel spreadsheet
         """
-        flag = self.valid_flag(line, ["-d", "-e"], 1, self.do_push_data.__doc__)
-        if type(flag) is int:
-            return flag
-        elif flag == "-d":
+        push_flag = self.valid_flag(line, ["-d", "-e"], 1, self.do_push_data.__doc__)
+        if type(push_flag) is int:
+            return push_flag
+        elif push_flag == "-d":
             view = DatabaseView()
             push_file = self.database_name
         else:
